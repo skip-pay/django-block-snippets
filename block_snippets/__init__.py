452d258795bbeb58cee7ba3354c87f302742cd3a
Petr Olah <donthave@email.cz>
2023-01-16 01:14:24 +0100

    Fork and publish.

--- block_snippets/__init__.py
+++ block_snippets/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (2, 0, 2)
+VERSION = (2, 0, 2, 1)
 
 
 def get_version():
--- example/dj/apps/app/tests/snippets.py
+++ example/dj/apps/app/tests/snippets.py
@@ -2,9 +2,8 @@ import json
 
 from django.utils.encoding import force_text
 
-from germanium.tools import assert_equal, assert_http_bad_request
+from germanium.tools import assert_equal, assert_http_not_found
 from germanium.test_cases.client import ClientTestCase
-from germanium.anotations import data_provider
 
 
 class SnippetTestCase(ClientTestCase):
@@ -68,4 +67,4 @@ class SnippetTestCase(ClientTestCase):
         assert_equal(resp['content-type'], 'text/plain')
 
     def test_bad_request_for_invalid_snippet_name_should_be_returned(self):
-        assert_http_bad_request(self.get(self.JSON_SNIPPET_URL + '?snippet=invalid'))
+        assert_http_not_found(self.get(self.JSON_SNIPPET_URL + '?snippet=invalid'))
